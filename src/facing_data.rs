1a3aa3076e45551c0cfe94bcd0103819d298e0a5
RonaldColyar <phantexcreations@gmail.com>
2021-09-07 22:24:42 -0500

    init

---
+++ src/facing_data.rs
@@ -0,0 +1,16 @@
+pub struct Facing{
+    different_bots:i32,
+    different_types:i32,
+    all_devices:i32,
+    config:Config,
+    contacts:Vec<String>,
+    connection_string:String,
+    banned_ips : Vec<String> 
+}
+
+pub struct Config{
+    deactivating:bool,
+    activating:bool,
+    disconnecting:bool,
+    viewing:bool
+}
