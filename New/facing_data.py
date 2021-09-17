b810a1cc9a1c98caa33c4a9f3a596bade7619afc
RonaldColyar <phantexcreations@gmail.com>
2021-09-17 06:47:02 -0500

    new additions

---
+++ New/facing_data.py
@@ -0,0 +1,29 @@
+class Facing:
+    def __init__(self,all_devices = None,config = None , contacts = None , banned_ips = None):
+        self.all_devices = all_devices
+        self.config = config  
+        self.contacts = contacts
+        self.banned_ips = banned_ips
+        self.num_of_non_bots = 0
+        self.num_of_bots = 0
+        self.num_of_contacts = 0
+        self.num_of_banned_ips = 0
+
+    def analyze_data_and_populate_instance(self):
+        self.num_of_banned_ips = len(self.banned_ips)
+        self.num_of_contacts = len(self.contacts)
+        self.gather_device_stats()
+
+    def gather_device_stats(self):
+        all_device_keys = self.all_devices.keys()
+        for key in all_device_keys:
+            if self.all_devices[key] == "non-bot":
+                self.num_of_non_bots += 1
+        self.num_of_bots = len(all_device_keys) - self.num_of_non_bots  
+            
+class Config:
+    def __init__(self,deactivating = None,activating = None,disconnecting = None,viewing = None):
+        self.deactivating = deactivating
+        self.activating = activating
+        self.disconnecting = disconnecting
+        self.viewing = viewing
---
+++ New/request_handler.py
@@ -0,0 +1,28 @@
+import json 
+
+class RequestHandler:
+    def __init__(self,websocket,parent):
+        self.parent = parent
+        self.websocket = websocket
+
+    async def handle_two_way_request(self,message):
+        await self.websocket.send(message)
+        response = self.websocket.recv()
+        basic_response = json.loads(response)
+
+        #send authentication credentials if needed
+        if basic_response["status"] == "needs-admin-auth":
+            password_to_send = None
+            if basic_response["action"] == "viewing":
+                password_to_send = self.parent.admin_password
+            else:
+                password_to_send = self.parent.super_admin_password
+
+            #return second response as string after auth attempt
+            await self.websocket.send(password_to_send)
+            second_response = await self.websocket.recv()
+            return second_response
+        else:
+            return response
+                
+
